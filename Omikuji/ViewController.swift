06816e187dbfb445789213eafdc7f4d43d17c1a2
sena.shimizu <senarugby0813@gmail.com>
2021-06-16 21:14:46 +0900

    first commit

---
+++ Omikuji/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Omikuji
+//
+//  Created by 清水世那 on 2021/06/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
